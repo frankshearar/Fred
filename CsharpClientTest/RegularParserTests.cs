4b9fa1020d161df6584e2d96708d9651b1aa1c9e
Frank Shearar <frank.shearar@gmail.com>
2014-10-09 16:57:26 +0100

    Fred.RegularParser is a C#-friendly wrapper for Fred.

---
+++ CsharpClientTest/RegularParserTests.cs
@@ -0,0 +1,63 @@
+﻿using Fred.RegularExpression;
+using NUnit.Framework;
+using System.Linq;
+
+namespace CsharpClient
+{
+    [TestFixture]
+    public class RegularParserTests
+    {
+        [Test]
+        public void CanCreateAlternateParser()
+        {
+            var p = RegularParser.Token('a').Or(RegularParser.Token('b'));
+            var matches = p.Match("abc").ToList();
+            Assert.AreEqual("a", matches[0]);
+            Assert.AreEqual("b", matches[1]);
+            Assert.AreEqual(2, matches.Count());
+        }
+
+        [Test]
+        public void CanCreateEmptyParser()
+        {
+            var p = RegularParser.Eps();
+            Assert.IsFalse(p.Match("abc").Any());
+        }
+
+        [Test]
+        public void CanCreateFailingParser()
+        {
+            var p = RegularParser.Empty();
+            Assert.IsFalse(p.Match("abc").Any());
+        }
+
+        [Test]
+        public void CanCreateKleeneStarParser()
+        {
+            var p = RegularParser.Token('a').Star();
+            var matches = p.Match("abaabbaaa").ToList();
+            Assert.AreEqual("a", matches[0]);
+            Assert.AreEqual("aa", matches[1]);
+            Assert.AreEqual("aaa", matches[2]);
+        }
+
+        [Test]
+        public void CanCreateSequenceParser()
+        {
+            var p = RegularParser.Token('a').Then(RegularParser.Token('b'));
+            var matches = p.Match("abab").ToList();
+            Assert.AreEqual("ab", matches[0]);
+            Assert.AreEqual("ab", matches[1]);
+            Assert.AreEqual(2, matches.Count);
+        }
+
+        [Test]
+        public void CanCreateTokenParser()
+        {
+            var p = RegularParser.Token('a');
+            var matches = p.Match("abc").ToList();
+            Assert.That(matches.Any());
+            Assert.AreEqual("a", matches.Single());
+        }
+    }
+}
---
+++ Fred/RegularParser.fs
@@ -0,0 +1,38 @@
+﻿namespace Fred.RegularExpression
+open Fred.Regex
+open System.Text
+
+// RegularParser provides a C# friendly wrapper around Regex.
+// At the moment it only supplies string-processing parsers.
+type RegularParser(parser: Parser<'a>) =
+    static member Empty() =
+        new RegularParser(Empty)
+    static member Eps() =
+        new RegularParser(Eps)
+    static member Token(o: System.IComparable) =
+        new RegularParser(Char o)
+    member this.Parser =
+        parser
+    member x.Match(s: System.String) =
+        // This complicated beast a String into
+        // Regex-friendly types, does the _one line of actual work_
+        // and then converts the output back to String.
+        let toString input =
+            let out = new StringBuilder()
+            input
+            |> List.map (fun (x: System.IComparable) -> x :?> System.Char)
+            |> Array.ofList
+            |> out.Append
+            |> ignore
+            out.ToString()
+        s.ToCharArray()
+        |> Array.map (fun c -> c :> System.IComparable)
+        |> List.ofArray
+        |> find parser
+        |> Seq.map (fun out -> toString out)
+    member x.Or(p: RegularParser) =
+        new RegularParser(Union(parser, p.Parser))
+    member x.Then(p: RegularParser) =
+        new RegularParser(Cat (parser,p.Parser))
+    member x.Star() =
+        new RegularParser(Star(parser))
