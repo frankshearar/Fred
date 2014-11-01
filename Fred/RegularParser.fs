c3ef96f065de482c7858aafc1b6ea595ddfc4c0e
Frank Shearar <frank.shearar@gmail.com>
2014-11-01 22:56:39 +0000

    Counting parsers (atMost, atLeast, rep) + character classes for C# wrapper.

--- Fred/RegularParser.fs
+++ Fred/RegularParser.fs
@@ -11,10 +11,27 @@ type RegularParser(parser: Parser<'a>) =
         new RegularParser(Eps)
     static member Token(o: System.IComparable) =
         new RegularParser(Char o)
+    static member Alpha with get() = let chars = seq { yield! seq {'A'..'Z'}
+                                                       yield! seq {'a'..'z'}}
+                                     RegularParser.Class chars
+    static member Num with get() = RegularParser.Class (seq {'0'..'9'})
+    static member Class(tokens: System.Collections.Generic.IEnumerable<System.Char>) =
+        let chars = tokens
+                    |> Seq.map (fun c -> c :> System.IComparable)
+                    |> List.ofSeq
+        new RegularParser(any chars)
     member this.Parser =
         parser
+    member x.AtLeast(n: int) =
+        new RegularParser(atLeast n x.Parser)
+    member x.AtMost(n: int) =
+        new RegularParser(atMost n x.Parser)
+    member x.Count(n: int) =
+        new RegularParser(rep n x.Parser)
+    member x.Count(min: int, max: int): RegularParser =
+        new RegularParser(reps min max x.Parser)
     member x.Match(s: System.String) =
-        // This complicated beast a String into
+        // This complicated beast turns a String into
         // Regex-friendly types, does the _one line of actual work_
         // and then converts the output back to String.
         let toString input =
@@ -32,6 +49,10 @@ type RegularParser(parser: Parser<'a>) =
         |> Seq.map (fun out -> toString out)
     member x.Or(p: RegularParser) =
         new RegularParser(Union(parser, p.Parser))
+    member x.Recognise(s: System.String) =
+        s.ToCharArray()
+        |> Array.map (fun c -> c :> System.IComparable)
+        |> matchSeq x.Parser
     member x.Then(p: RegularParser) =
         new RegularParser(Cat (parser,p.Parser))
     member x.Star() =
