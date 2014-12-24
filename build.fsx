362456f6f1df5f1b8c032567fe83ba6391482547
Frank Shearar <frank.shearar@gmail.com>
2014-12-24 12:52:14 +0000

    Now we can create NuGet packages from the build script.

--- build.fsx
+++ build.fsx
@@ -3,29 +3,69 @@
 open Fake
 open Fake.AssemblyInfoFile
 open Fake.Git
+open System.IO
+open System.Xml
 
 RestorePackages()
 
 let buildDir = "./build/"
 let testDir = "./test/"
 
+let authors = ["Frank Shearar"]
+let projectName = "Fred"
+let summary = "Parsing with derivatives"
+
 let version =
     match buildServer with
     | AppVeyor -> buildVersion
-    | _ -> "0.0.0.1"
+    | _ -> "0.1.0.0"
 
 let commitHash = Information.getCurrentHash()
 
+// Nicked from UnionArgParser. Thanks, guys!
+let addAssembly (target : string) assembly =
+    printfn "addAssembly %A %A" target assembly
+    let includeFile force file =
+        let file = file
+        if File.Exists (Path.Combine("nuget", file)) then [(file, Some target, None)]
+        elif force then raise <| new FileNotFoundException(file)
+        else []
+
+    seq {
+        yield! includeFile true assembly
+        yield! includeFile false <| Path.ChangeExtension(assembly, "pdb")
+        yield! includeFile false <| Path.ChangeExtension(assembly, "xml")
+        yield! includeFile false <| assembly + ".config"
+    }
+
+// Turn the packages.config into something we can inject
+// into the nuspec template.
+let depsFromPackagesConfig (f: FileInfo) =
+    let xml = new XmlDocument()
+    let nsMgr = new XmlNamespaceManager(xml.NameTable)
+    xml.Load(XmlReader.Create(f.FullName))
+    xml.SelectNodes("//package", nsMgr)
+    |> Seq.cast<XmlNode>
+    |> Seq.map (fun (pkgNode: XmlNode) -> pkgNode.Attributes.["id"].Value, pkgNode.Attributes.["version"].Value)
+
+let dependenciesFrom projectName =
+    let projectDir = Path.Combine [|currentDirectory; projectName|]
+    let pkgs = (new DirectoryInfo(projectDir)).GetFiles("packages.config").[0]
+    depsFromPackagesConfig pkgs
+    |> List.ofSeq
+
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; testDir]
+    CleanDirs [buildDir; testDir; "nugetpackages"]
 )
+
+Target "RestorePackages" RestorePackages
 
 Target "SetAssemblyInfo" (fun _ ->
     let assemblyPath = "./Fred/Properties/AssemblyInfo.cs"
     System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(assemblyPath)) |> ignore
     CreateCSharpAssemblyInfo assemblyPath
-        [Attribute.Title "Fred"
-         Attribute.Description "Parsing with derivatives"
+        [Attribute.Title projectName
+         Attribute.Description summary
          Attribute.Guid "6F3B81EE-EA0E-4546-B722-A7CEB6C73208"
          Attribute.Product "Fred"
          Attribute.Metadata("githash", commitHash)
@@ -37,6 +77,25 @@ Target "BuildSolution" (fun _ ->
     MSBuildWithDefaults "Build" ["Fred.sln"]
     |> Log "AppBuild-Output: "
 )
+
+Target "CreatePackage" (fun _ ->
+    // This is WAY too much effort. Surely there's an easier way?!
+    CreateDir @"nuget\lib\net45"
+    CopyFile @"nuget\lib\net45" @"Fred\bin\Release\Fred.dll"
+
+    NuGet (fun p ->
+        {p with
+            Authors = authors
+            Project = projectName
+            Description = summary
+            OutputPath = "nuget"
+            Summary = summary
+            WorkingDir = "nuget"
+            Version = version
+            Publish = false
+            Dependencies = getDependencies "Fred/packages.config"
+        })
+        "nuget/Fred.nuspec")
 
 Target "Test" (fun _ ->
     !! "./FredTest/bin/Release/FredTest.dll" // Don't like that "Release" there, but we need to limit ourselves to _one_ copy of the test dll
